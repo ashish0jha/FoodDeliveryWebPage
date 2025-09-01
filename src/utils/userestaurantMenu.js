59d7a67b35382d70e0ae23c70c8d66f59c7ab54b
ashish0jha <ashish2893jnv@gmail.com>
2025-09-01 16:45:33 +0530

    First Update

--- src/utils/userestaurantMenu.js
+++ src/utils/userestaurantMenu.js
@@ -10,7 +10,11 @@ const useRestaurantMenu=(resId)=>{
 
     const fetchData=async ()=>{
         const data=await fetch(
-            "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.7040592&lng=77.10249019999999&restaurantId="+resId+"&catalog_qa=undefined&submitAction=ENTER");
+            "https://proxy.cors.sh/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.7040592&lng=77.10249019999999&restaurantId="+resId+"&catalog_qa=undefined&submitAction=ENTER",{
+                 headers:{
+                    'x-cors-api-key':"temp_a09aeb4da8968b2402cf6a9e6253ecb1"
+                }
+            });
 
         const json=await data.json();
 
