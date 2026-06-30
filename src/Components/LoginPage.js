5e07c205ae7e0046967f52ce32ad0cfc76f81c97
ashish0jha <ashish2893jnv@gmail.com>
2026-06-30 19:41:01 +0530

    Updated the login page

--- src/Components/LoginPage.js
+++ src/Components/LoginPage.js
@@ -11,8 +11,8 @@ const LoginPage = () => {
   const navigate = useNavigate();
 
   const [fullName, setFullName] = useState("");
-  const [email, setEmail] = useState("ashish@gmail.com");
-  const [password, setPassword] = useState("Ashish@123");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [error, setError] = useState("");
 
   const { LoggedIn, setUserName } = useContext(UserContext);
