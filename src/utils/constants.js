e6669647ef2dfd6fca3e1f41087f32b029c363d7
ashish0jha <ashish2893jnv@gmail.com>
2026-06-30 19:00:01 +0530

    just a small change

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,6 @@
-export const baseUrl = "http://localhost:8009"
+export const baseUrl = window.location.hostname === "localhost"
+    ? "http://localhost:8009"
+    : "https://fooddelivery-backend-gk18.onrender.com";
 
 export const LOGO_Link=
     "https://i.postimg.cc/cL2vLLBK/download-removebg-preview.png";
